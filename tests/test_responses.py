ac498b307fe3f7335814ab8e877bb6421ed1f5b8
David Fialho <fialho.david@protonmail.com>
2017-09-16 19:18:08 +0100

    Organize code into separate packages

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,7 +1,7 @@
 from hypothesis import given
 from hypothesis.strategies import builds, text, sampled_from
 
-from responses import Response, ErrorCode, rpc_response
+from communication.responses import Response, ErrorCode, rpc_response
 
 
 # noinspection PyUnresolvedReferences
